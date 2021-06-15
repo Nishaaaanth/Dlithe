cf36b80933530946b7388ba008acf1b93aecb20a
Nishanth Kiran <83387409+Nishaaaanth@users.noreply.github.com>
2021-06-15 20:21:55 +0530

    Create LeapYear.py

---
+++ LeapYear.py
@@ -0,0 +1,29 @@
+'''
+An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+In the Gregorian calendar, three conditions are used to identify leap years:
+The year can be evenly divided by 4, is a leap year, unless:
+The year can be evenly divided by 100, it is NOT a leap year, unless:
+The year is also evenly divisible by 400. Then it is a leap year.
+This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.
+
+Given a year, determine whether it is a leap year. If it is a leap year, print YES or NO.
+Input Format
+Read year, the year to test.
+
+Output Format
+It should YES or NO
+
+Sample Input 0
+1990
+
+Sample Output 0
+NO
+'''
+
+y = int(input("Enter a Year: "))
+
+if (y%4 == 0 and y%100 != 0) or y%400 == 0:
+    print("YES")
+else:
+    print("NO")
